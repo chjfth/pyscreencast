3109818838924243944e06b47cb39babce93ee2e
chjhome <chjfth@hotmail.com>
2019-10-29 21:13:14 +0800

    Bundle Python27.zip and unzip.exe so that start.bat will automatically extract Python27 in-place and use it.

--- start.bat
+++ start.bat
@@ -5,6 +5,12 @@ set batdir=%batdir:~0,-1%
 
 PATH=%batdir%\Python27
 
+pushd %batdir%
+if not exist "Python27" (
+	echo Extracting Python27...
+	unzip Python27.zip
+)
+
 python %batdir%\pyscreencast\pyscreencast.py
 
 rem echo %batdir%
